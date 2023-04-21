c16cf94f9edbb3016fab21f9aceec4f929b86271
Victor Leina Ngeny <victorleinangeny@gmail.com>
2023-04-21 16:03:23 +0300

    lab attempted

--- src/components/App.js
+++ src/components/App.js
@@ -5,10 +5,23 @@ console.log(blogData);
 
 function App() {
   return (
+    <>
+  
     <div className="App">
+
       You're on your own from here! Follow the deliverables; test things out in
       the browser as you write your code; and good luck!
+
     </div>
+    <div>
+    <header>
+       <h1>{props.blogData}</h1>
+    </header>
+    
+    image = "https://www.bing.com/images/search?view=detailV2&ccid=U9RwLKzO&id=94FD58DE7C9A148B956C11E448F1CC961992BD4A&thid=OIP.U9RwLKzOyhpIKgpSCuBWEAHaEK&mediaurl=https%3a%2f%2fi.ytimg.com%2fvi%2flhOlOFxpeSs%2fmaxresdefault.jpg&cdnurl=https%3a%2f%2fth.bing.com%2fth%2fid%2fR.53d4702cacceca1a482a0a520ae05610%3frik%3dSr2SGZbM8UjkEQ%26pid%3dImgRaw%26r%3d0&exph=720&expw=1280&q=Random+Images+Generator&simid=608011226063525856&FORM=IRPRST&ck=DD6F3D7B047BF6AE306EBABE9A79E14B&selectedIndex=0&ajaxhist=0&ajaxserp=0;"/>
+    
+    </div>
+    </>
   );
 }
 
